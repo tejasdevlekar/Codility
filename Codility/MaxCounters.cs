5fb56d6d5c538c8c83f896674db221d51be457bf
tejasdevlekar <tejasdevlekar@users.noreply.github.com>
2020-12-28 21:33:18 +0530

    MaxCounters solution2

--- Codility/MaxCounters.cs
+++ Codility/MaxCounters.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Codility
 {
@@ -12,7 +13,45 @@ namespace Codility
             int N = 5;
             int[] A = new int[] { 3, 4, 4, 6, 1, 4, 4 };
 
-            solution(N, A);
+            int[] result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
+
+            A = new int[] { 3, 4, 4, 6, 1, 4, 6 };
+
+            result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
+
+            A = new int[] { 6, 6, 6, 6, 6, 6, 6 };
+
+            result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
+
+            A = new int[] { 6,6,6,6,6,6,6,3, 4, 4, 6, 1, 4, 6,6,6,6,6,6,6,6 };
+
+            result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
+
+            A = new int[] { 6, 6, 6, 6, 6, 6, 6, 3, 4, 4, 6, 1, 4, 4, 6, 6, 6, 6, 6, 6, 6 };
+
+            result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
+
+            A = new int[] { 6, 6, 6, 6, 6, 6, 6, 3, 4, 4, 6,6,6,6,6,6,6,6,6, 1, 4, 6, 6, 6, 6, 6, 6, 6, 6 };
+
+            result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
+
+            A = new int[] { 6, 6, 6, 6, 6, 6, 6, 3, 4, 4, 6,6,6,6,6,6,6,6,6,6, 1, 4, 4, 6, 6, 6, 6, 6, 6, 6 };
+
+            result = solution2(N, A);
+            result.ToList().ForEach(r => Console.Write($"{r} "));
+            Console.WriteLine();
         }
 
         public int[] solution(int N, int[] A)
@@ -37,5 +76,43 @@ namespace Codility
 
             return counterArray;
         }
+
+        public int[] solution2(int N, int[] A)
+        {
+            int[] array = A;
+            int[] count = new int[N];
+            int[] tmpcount = new int[N];
+            int maxVal = 0;
+            int tmpMaxVal = 0;
+            foreach (int num in array)
+            {
+                if (num > N)
+                {
+                    if (tmpMaxVal > 0)
+                    {
+                        maxVal += tmpMaxVal;
+                        count = new int[N];
+                        tmpMaxVal = 0;
+                    }
+                }
+                else
+                {
+                    count[num-1]++;
+                    tmpMaxVal = Math.Max(tmpMaxVal, count[num - 1]);
+                }
+            }
+
+            if (maxVal > 0)
+            {
+                for (int i = 0; i < count.Length; i++)
+                {
+                    count[i] += maxVal;
+                }
+            }
+
+            return count;
+        }
+
+
     }
 }
